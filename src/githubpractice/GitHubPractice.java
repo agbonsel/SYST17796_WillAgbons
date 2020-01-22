c1939af32176d7a9e7edd05b14eef912c53a844e
agbonsel <agbonsel@sheridancollege.ca>
2020-01-22 17:57:22 -0500

    initial commit for new student code

--- src/githubpractice/GitHubPractice.java
+++ src/githubpractice/GitHubPractice.java
@@ -12,7 +12,7 @@ public class GitHubPractice {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-      
+      System.out.println("Hello");
     }
 
 }
