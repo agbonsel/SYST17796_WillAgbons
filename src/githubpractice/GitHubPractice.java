c6c645b0fb4a7b656c45d45a1741b240fccc9c79
agbonsel <agbonsel@sheridancollege.ca>
2020-01-22 17:49:40 -0500

    initial commit for new student code

---
+++ src/githubpractice/GitHubPractice.java
@@ -0,0 +1,18 @@
+
+package githubpractice;
+
+/**
+ * This class +++Insert Description Here+++
+ *
+ * @author Will Agbonselobho
+ */
+public class GitHubPractice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+    }
+
+}
